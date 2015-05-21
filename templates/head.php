c2d0d71627fa7e23b23789519e8783181a8ba2f8
Tim Samuelsson <tim.samuelsson@gmail.com>
2015-05-21 10:39:27 +0200

    Adding standard classes to prevent Flash of unstyled content

--- templates/head.php
+++ templates/head.php
@@ -2,5 +2,15 @@
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    
     <?php wp_head(); ?>
+    
+    <style>
+        .init-hide { display: none; }
+    </style>
+    <noscript>
+        <style>
+            .init-hide { display: block !important; }
+        </style>
+    </noscript>
 </head>
