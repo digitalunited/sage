4cb18dc84c41ba074638f268b0b9d3c8b16f4b5e
Lucas <lucas@careofhaus.se>
2015-08-24 11:05:47 +0200

    Image module now uses responsive image and footer component cleaned.

--- components/Footer/view.php
+++ components/Footer/view.php
@@ -1,50 +1,6 @@
 <footer class="content-info" role="contentinfo">
     <div class="container">
         <div class="row">
-            <div class="col-xs-12">
-                <div class="row">
-                    <div class="col-xs-12 col-sm-4 col-md-2 social">
-                        <h3><?= __('social.media.links.headline.footer', 'components') ?></h3>
-                        <hr>
-                        <?php
-                        $fbUrl = get_field('facebook-url', 'option');
-                        $linkedInUrl = get_field('linkedin-url', 'option');
-                        $mynewsDeskUrl = get_field('mynewsdesk-url', 'option');
-
-                        if ($fbUrl) { ?>
-                            <a target="_blank" href="<?= $fbUrl ?>" title="<?= __('find.us.on.facebook', 'components') ?>"><?=__('find.us.on.facebook', 'components') ?><i class="icon icon-facebook"></i></a>
-                        <?php
-                        }
-                        ?>
-
-                        <?php if ($linkedInUrl) { ?>
-                            <a target="_blank" href="<?= $linkedInUrl ?>" title="<?= __('find.us.on.linked.in', 'components') ?>"><?= __('find.us.on.linked.in', 'components') ?><i class="icon icon-linkedin"></i></a>
-                        <?php
-                        }
-                        ?>
-
-                        <?php if ($mynewsDeskUrl) { ?>
-                            <a target="_blank" href="<?= $mynewsDeskUrl ?>" title="<?= __('find.us.on.mynewsdesk', 'components') ?>"><?= __('find.us.on.mynewsdesk', 'components') ?><i class="icon icon-mynewsdesk"></i></a>
-                        <?php
-                        }
-                        ?>
-
-                    </div>
-                </div>
-            </div>
-            <div class="col-xs-12 copyright">
-                <div class="row">
-                    <div class="col-xs-12">
-                        <hr/>
-                    </div>
-                    <div class="col-xs-12 col-sm-9 col-md-8">
-                        <?= wpautop(get_field('copyright-text', 'option')) ?>
-                    </div>
-                    <div class="col-xs-12 col-sm-3 col-md-4">
-                        <i class="icon-logo"></i>
-                    </div>
-                </div>
-            </div>
         </div>
     </div>
 </footer>
--- components/Image/component.php
+++ components/Image/component.php
@@ -10,43 +10,59 @@ class Image extends \DigitalUnited\Components\VcComponent
     protected function getComponentConfig()
     {
         return [
-                'name' => __('component.name.image', 'components'),
-                'params' => [
-                        [
-                                "type" => "vc_link",
-                                "holder" => "",
-                                "admin_label" => false,
-                                "class" => "",
-                                "heading" => __("admin.text.link", "components"),
-                                "param_name" => "link",
-                                "value" => "",
-                                "description" => __("admin.text.field.may.be.blank", "components"),
-                        ],
-                        [
-                                "type" => "attach_image",
-                                "holder" => "",
-                                "class" => "",
-                                "heading" => __("admin.text.image", "components"),
-                                "admin_label" => false,
-                                "param_name" => "image_id",
-                                "value" => "",
-                                "description" => ""
-                        ],
-                        [
-                                "type" => "dropdown",
-                                "holder" => "",
-                                "heading" => __("admin.text.theme", "components"),
-                                "admin_label" => false,
-                                "param_name" => "theme",
-                                "value" => [
-                                    __('admin.text.standard', 'components') => 'standard',
-                                    __('admin.text.theme.no.margin', 'components') => 'no-margin'
-                                ],
-                                "std" => 'standard',
-                                "description" => ""
-                        ],
+            'name' => __('component.name.image', 'components'),
+            'params' => [
+                [
+                    "type" => "vc_link",
+                    "holder" => "",
+                    "admin_label" => false,
+                    "class" => "",
+                    "heading" => __("admin.text.link", "components"),
+                    "param_name" => "link",
+                    "value" => "",
+                    "description" => __("admin.text.field.may.be.blank", "components"),
                 ],
-                "icon" => "icon-wpb-single-image"
+                [
+                    "type" => "attach_image",
+                    "holder" => "",
+                    "class" => "",
+                    "heading" => __("admin.text.image", "components"),
+                    "admin_label" => false,
+                    "param_name" => "image_id",
+                    "value" => "",
+                    "description" => ""
+                ],
+                [
+                    "type" => "dropdown",
+                    "holder" => "",
+                    "heading" => __("admin.text.theme", "components"),
+                    "admin_label" => false,
+                    "param_name" => "theme",
+                    "value" => [
+                        __('admin.text.standard', 'components') => 'standard',
+                        __('admin.text.theme.no.margin', 'components') => 'no-margin'
+                    ],
+                    "std" => 'standard',
+                    "description" => ""
+                ],
+                [
+                    "type" => "dropdown",
+                    "holder" => "div",
+                    "heading" => __("admin.aspect-ratio.heading", "components"),
+                    "admin_label" => false,
+                    "param_name" => "ratio",
+                    "value" => [
+                        'Oförändrad' => 'none',
+                        '1x1 (Fyrkantig)' => '1x1',
+                        '2x1 (Landskap)' => '2x1',
+                        '4x3 (Landskap)' => '4x3',
+                        '16x9 (Landskap)' => '16x9',
+                    ],
+                    "std" => 'none',
+                    "description" => __("admin.aspect-ratio.description", "components")
+                ]
+            ],
+            "icon" => "icon-wpb-single-image"
         ];
     }
 
@@ -54,20 +70,27 @@ class Image extends \DigitalUnited\Components\VcComponent
     {
         $data['image'] = $data['image_id'] ? $this->getImage($data['image_id']) : '';
         $data['link'] = new \DigitalUnited\Components\Link($data['link']);
+
         return $data;
     }
 
-    protected function getImage($imgId) {
-        $attachment = wp_get_attachment_image_src($imgId, 'large');
-        $src = $attachment[0];
-
+    protected function getImage($imgId)
+    {
         $alt = $this->getImgAltTag($imgId);
 
-        return "<img class='img-responsive' src='{$src}' alt='{$alt}' />";
+        return \DigitalUnited\ResponsiveImage::render([
+            'imgId' => $imgId,
+            'output' => 'img',
+            'ratio' => $this->param('ratio'),
+            'imgAttributes' => [
+                'alt' => $alt,
+            ],
+        ]);
     }
 
-    protected function getImgAltTag($imgId) {
-        return trim(strip_tags( get_post_meta($imgId, '_wp_attachment_image_alt', true)));
+    protected function getImgAltTag($imgId)
+    {
+        return trim(strip_tags(get_post_meta($imgId, '_wp_attachment_image_alt', true)));
     }
 
     public function main()
@@ -75,6 +98,7 @@ class Image extends \DigitalUnited\Components\VcComponent
     }
 }
 
-function getImage($params) {
+function getImage($params)
+{
     return (new Image($params))->render();
 }
